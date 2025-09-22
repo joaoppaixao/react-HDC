89e32dc0c929b4cf593b29395833005aa562d6b4
João Augusto Palma Paixão <joaoaugustopp@gmail.com>
2025-09-22 14:51:51 -0300

    evento com função

--- 2_FUNDAMENTOS/vite-project/src/components/Events.jsx
+++ 2_FUNDAMENTOS/vite-project/src/components/Events.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 
 const Events = () => {
+
+    const handleClick = (e) => {
+        console.log(e)
+        console.log("Executou")
+    }
+
   return (
     <div>
         <button onClick={() => console.log("Testando um evento")}>Clique aqui</button>
+        {/* 7 - evento com função */}
+        <div>
+            <button onClick={handleClick}>Clique aqui - com função</button>
+        </div>
     </div>
   )
 }
