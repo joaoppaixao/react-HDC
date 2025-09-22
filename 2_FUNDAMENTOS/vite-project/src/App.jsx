741a48dc513ee91af8356141912d3587ffe26942
João Augusto Palma Paixão <joaoaugustopp@gmail.com>
2025-09-22 14:20:04 -0300

    comentarios no JSX

--- 2_FUNDAMENTOS/vite-project/src/App.jsx
+++ 2_FUNDAMENTOS/vite-project/src/App.jsx
@@ -1,8 +1,12 @@
 import './App.css'
+
+// 2 - importando o componente
 import FirstComponent from './components/FirstComponent';
 
 function App() {
+  // 3 - comentários
   return <div className='App'>
+    {/* 3 - comentários no JSX */}
     <h1>Fundamentos do React</h1>
     <FirstComponent></FirstComponent>
   </div>;
--- 2_FUNDAMENTOS/vite-project/src/components/FirstComponent.jsx
+++ 2_FUNDAMENTOS/vite-project/src/components/FirstComponent.jsx
@@ -1,3 +1,5 @@
+// 1 - criando o componente
+
 const FirstComponent = () => {
     return (
         <div>
