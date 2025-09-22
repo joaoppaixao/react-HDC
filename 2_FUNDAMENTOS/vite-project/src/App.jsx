b646dd6026a00071fc771555ceabeec2ccfd75fc
João Augusto Palma Paixão <joaoaugustopp@gmail.com>
2025-09-22 14:31:58 -0300

    template expression em jsx

--- 2_FUNDAMENTOS/vite-project/src/App.jsx
+++ 2_FUNDAMENTOS/vite-project/src/App.jsx
@@ -2,13 +2,15 @@ import './App.css'
 
 // 2 - importando o componente
 import FirstComponent from './components/FirstComponent';
+import TemplateExpression from './components/TemplateExpression';
 
 function App() {
   // 3 - comentários
   return <div className='App'>
     {/* 3 - comentários no JSX */}
     <h1>Fundamentos do React</h1>
-    <FirstComponent></FirstComponent>
+    <FirstComponent />
+    <TemplateExpression />
   </div>;
 }
 
---
+++ 2_FUNDAMENTOS/vite-project/src/components/TemplateExpression.jsx
@@ -0,0 +1,15 @@
+// 4 - template expression
+
+const TemplateExpression = () => {
+
+    const name = "João";
+
+  return (
+    <div>
+        <p>A soma é: {2 + 2}</p>
+        <h3>Bem-vindo, {name}.</h3>
+    </div>
+  );
+}
+
+export default TemplateExpression
