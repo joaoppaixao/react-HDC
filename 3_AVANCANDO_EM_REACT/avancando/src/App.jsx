ffa672bcdf80597ab2ca7f4244ecdef41b32a456
João Augusto Palma Paixão <joaoaugustopp@gmail.com>
2025-09-25 13:59:49 -0300

    importando imagens das pastas assets e public

--- 3_AVANCANDO_EM_REACT/avancando/src/App.jsx
+++ 3_AVANCANDO_EM_REACT/avancando/src/App.jsx
@@ -1,11 +1,17 @@
 import './App.css'
 
+// 2 - imagem em assets
+import night from './assets/night.jpg';
+
 function App() {
 
   return (
     <div className='App'>
       <h1>Avançando em React</h1>
+      {/* 1 - imagem em public */}
       <img src="img.jpg" alt="Alguma imagem" />
+      {/* 2 - imagem em assets */}
+      <img src={night} alt="Outra imagem" />
     </div>
   )
 }
