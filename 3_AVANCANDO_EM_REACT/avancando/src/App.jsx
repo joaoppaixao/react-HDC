773740e01d7b4130dad839729ad948be1a707283
João Augusto Palma Paixão <joaoaugustopp@gmail.com>
2025-10-06 10:44:04 -0300

    conhecendo o hook useState

--- 3_AVANCANDO_EM_REACT/avancando/src/App.jsx
+++ 3_AVANCANDO_EM_REACT/avancando/src/App.jsx
@@ -3,15 +3,20 @@ import './App.css'
 // 2 - imagem em assets
 import night from './assets/night.jpg';
 
+// 3 - useState
+import Data from './components/Data';
+
 function App() {
 
   return (
-    <div className='App'>
+    <div className='App' style={{ paddingBottom: "500px"}}>
       <h1>Avançando em React</h1>
       {/* 1 - imagem em public */}
       <img src="img.jpg" alt="Alguma imagem" />
       {/* 2 - imagem em assets */}
       <img src={night} alt="Outra imagem" />
+      {/* 3 - useState */}
+      <Data />
     </div>
   )
 }
