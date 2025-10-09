fc67ee3b9c4754f9f44589871a8090356d8b0523
João Augusto Palma Paixão <joaoaugustopp@gmail.com>
2025-10-09 11:46:54 -0300

    propriedade key

--- 3_AVANCANDO_EM_REACT/avancando/src/components/ListRender.jsx
+++ 3_AVANCANDO_EM_REACT/avancando/src/components/ListRender.jsx
@@ -4,12 +4,25 @@ const ListRender = () => {
 
     const [list] = useState(["João", "Bruna", "Barata", "Ana"]);
 
+    const [users, setUsers] = useState([
+      {id: 1, nome: "João", age: 20},
+      {id: 2, nome: "Bruna", age: 20},
+      {id: 3, nome: "Barata", age: 21},
+      {id: 4, nome: "Ana", age: 21}
+    ])
   return (
   <div>
+    {/* 4 - render sem key */}
     <ul>
         {list.map((item) => (
             <li>{item}</li>
         ))}
+    </ul>
+    {/* 5 - render com key */}
+    <ul>
+      {users.map((user) => (
+        <li key={user.id}>{user.nome} - {user.age} anos</li>
+      ))}
     </ul>
   </div>
   );
