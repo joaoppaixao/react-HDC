68466bcc6d4416e38de7b5a123d18cac1a7ddd62
João Augusto Palma Paixão <joaoaugustopp@gmail.com>
2025-10-14 14:50:49 -0300

    adicionando um else nas condições

--- 3_AVANCANDO_EM_REACT/avancando/src/components/ConditionalRender.jsx
+++ 3_AVANCANDO_EM_REACT/avancando/src/components/ConditionalRender.jsx
@@ -3,11 +3,24 @@ import React from 'react'
 const ConditionalRender = () => {
     const x = 5;
 
+    const name = "João"
+
   return (
     <div>
         {/* 7 - render condicional */}
         <h3>Isso será exibido?</h3>
         {x > 2 && <p>Se x for maior que 2, sim!</p>}
+        {/* 8 - else */}
+        <h3>Render ternário</h3>
+        {name === "João" ?(
+            <div>
+                <p>Olá, João!</p>
+            </div>
+        ) : (
+            <div>
+                <p>Nome não encontrado!</p>
+            </div>
+        )}
     </div>
   )
 }
