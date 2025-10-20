600e1945d52d6dc547637dad6d861240536cef58
João Augusto Palma Paixão <joaoaugustopp@gmail.com>
2025-10-20 12:38:49 -0300

    css de componentes

--- 4_CSS_REACT/css-react/src/App.jsx
+++ 4_CSS_REACT/css-react/src/App.jsx
@@ -1,11 +1,17 @@
 import './App.css'
 
+// 2 - css do componente
+import MyComponent from './components/MyComponent'
+
 function App() {
 
   return (
     <div>
       { /* 1 - css global */}
       <h1>CSS no React</h1>
+      { /* 2 - css do componente */}
+      <MyComponent />
+      <p>Pegou o CSS do componente</p>
     </div>
   )
 }
