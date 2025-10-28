8232cf4f6d3f3090a5cd1acb14349e56f36b65a7
João Augusto Palma Paixão <joaoaugustopp@gmail.com>
2025-10-28 11:55:53 -0300

    funcionalidade de limpar formulario implementada

--- projects/calculadora-imc/src/components/ImcCalc.jsx
+++ projects/calculadora-imc/src/components/ImcCalc.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import { useState } from "react";
+
+import Button from './Button'
+
 import './ImcCalc.css'
 
 const ImcCalc = () => {
+
+    const [height, setHeight] = useState("");
+    const [weight, setWeight] = useState("");
+
+    const clearForm = (e) => {
+        e.preventDefault();
+        setWeight("");
+        setHeight("");
+    }
+
   return (
     <div id='calc-container'>
         <h2>Calculadora de IMC</h2>
@@ -9,16 +22,18 @@ const ImcCalc = () => {
             <div className="form-inputs">
                 <div className="form-control">
                     <label htmlFor="height">Altura:</label>
-                    <input type="text" name='height' id='height' placeholder='Exemplo 1,75'/>
+                    <input type="text" name='height' id='height' placeholder='Exemplo 1,75' 
+                    onChange={(e) => setHeight(e.target.value)} value={height}/>
                 </div>
                 <div className="form-control">
                     <label htmlFor="weight">Peso:</label>
-                    <input type="text" name='weight' id='weight' placeholder='Exemplo 70,5'/>
+                    <input type="text" name='weight' id='weight' placeholder='Exemplo 70,5'
+                    onChange={(e) => setWeight(e.target.value)} value={weight}/>
                 </div>
             </div>
             <div className="action-control">
-                <button>Calcular</button>
-                <button>Limpar</button>
+                <Button id="calc-btn" text="Calcular" />
+                <Button id="clear-btn" text="Limpar" action={clearForm} />
             </div>
         </form>
     </div>
